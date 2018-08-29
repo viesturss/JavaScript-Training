447ccf0128d3c56ff8c6173c5976c42ef093bf7a
GuskiS <edvards@lazdans.lv>
2018-08-29 17:52:44 +0300

    Add 1st task

---
+++ tasks/1.game_of_life.js
@@ -0,0 +1,8 @@
+// Write Game of Life application that works with different providers.
+// Current provider will be called TestProvider.
+// That means you have to unit test your application, preferably with TDD approach.
+// Create files in `src` folder. Use JEST for testing the application.
+
+// Resources:
+// https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life
+// https://pmav.eu/stuff/javascript-game-of-life-v3.1.1/
