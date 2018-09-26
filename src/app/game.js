b9bf47a9f361e38e259621d1c2c4209a45cf9145
Viesturs Priedītis <viesturs.pr@gmail.com>
2018-09-26 23:20:40 +0300

    Add changelog logic

--- src/app/game.js
+++ src/app/game.js
@@ -3,9 +3,9 @@ import { DOMProvider } from "./../providers";
 
 const game = new GameOfLife({
   provider: new DOMProvider(),
-  sizeX: 100,
-  sizeY: 50,
-  speed: 100
+  sizeX: 5,
+  sizeY: 5,
+  speed: 1000
 });
 
 game.start();
--- src/providers/DOMProvider.js
+++ src/providers/DOMProvider.js
@@ -1,10 +1,38 @@
 export default class DOMProvider {
-  constructor() {}
+  constructor() {
+    this._changeLogBodyElement = document.querySelector("#changelog>tbody");
+    this.changeLog = [];
+    this.iterationChanges = [1, 0, 0, 0, 0];
+  }
 
-  onIteration = () => {};
-  onIsolation = () => {};
-  onLive = () => {};
-  onOverPopulation = () => {};
-  onReproduction = () => {};
-  grid = () => {};
+  onIteration = grid => {
+    this.changeLog.push(this.iterationChanges);
+    this._renderChangeLog();
+    this.iterationChanges = [++this.iterationChanges[0], 0, 0, 0, 0];
+  };
+  onIsolation = () => {
+    this.iterationChanges[1]++;
+  };
+  onLive = () => {
+    this.iterationChanges[2]++;
+  };
+  onOverPopulation = () => {
+    this.iterationChanges[3]++;
+  };
+  onReproduction = () => {
+    this.iterationChanges[4]++;
+  };
+  grid = (colCount, rowCount) => {
+    return [...Array(rowCount)].map(() => {
+      return [...Array(colCount)].map(() => Math.random() < 0.3);
+    });
+  };
+
+  _renderChangeLog() {
+    const row = this._changeLogBodyElement.insertRow(0);
+    this.iterationChanges.forEach(count => {
+      const cell = row.insertCell();
+      cell.textContent = count;
+    });
+  }
 }
