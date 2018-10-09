a4e4bb46d04858ec7fa7505fa660f62d85f0ad95
GuskiS <edvards@lazdans.lv>
2018-10-09 19:17:30 +0300

    LESSON-3

---
+++ tasks/3.react.js
@@ -0,0 +1,8 @@
+// Write a new provider that will render React components.
+// Add validations for inputs:
+//   size: 5 < n <= 1000
+//   speed: 1 < n <= 5000
+// Use Material UI components for styling.
+//
+// Resources:
+// https://reactjs.org/tutorial/tutorial.html
